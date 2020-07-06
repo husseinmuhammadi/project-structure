8a4266b7259bdb7d6638a8e5a3028a9d621a4e6a
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2020-07-06 00:40:00 -0700

    resource-filtering

---
+++ resource-filtering/src/main/java/com/javastudio/tutorial/pom/Application.java
@@ -0,0 +1,40 @@
+package com.javastudio.tutorial.pom;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println();
+        // dumpSystemProperties();
+        dumpReleaseSpecifications();
+    }
+
+    private static void dumpReleaseSpecifications() {
+        try (InputStream resource = Application.class.getClassLoader().getResourceAsStream("version.properties")) {
+            Properties p = new Properties();
+            p.load(resource);
+            System.out.println(p.getProperty("release.artifactId"));
+            System.out.println(p.getProperty("release.version"));
+            System.out.println(p.getProperty("java.version"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void dumpSystemProperties() {
+        Properties p = System.getProperties();
+        Set set = p.entrySet();
+
+        Iterator itr = set.iterator();
+        while (itr.hasNext()) {
+            Map.Entry entry = (Map.Entry) itr.next();
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
+    }
+}
